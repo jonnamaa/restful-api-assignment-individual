504d35490d2ca9741bcddb6554b1d8987cbbcc26
jonnamaa <jonna1805@student.hamk.fi>
2023-04-19 18:54:09 +0300

    id and getOne added

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+// GET ALL
 app.get('/my-bucketlist', (req, res) => {
     //res.send("Testing");
     res.render('my-bucketlist',
@@ -40,6 +41,42 @@ app.get('/my-bucketlist', (req, res) => {
        bucketlist : bucketlist 
     });
 })
+
+// GET ONE
+app.get('/my-bucketlist/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const item = bucketlist.find(item => item.id === id);
+
+    if(item){
+        //res.json(item);
+        res.render('my-bucketlist', {
+            //product : product.toJSON()
+            pagetitle : "This is a bucketlist item!",
+            bucketlist : bucketlist 
+        });
+    }
+    else{
+        res.status(404).json(
+            {
+                msg: 'Not found'
+            }
+        )
+    }
+});
+
+// CREATE 
+
+
+// UPDATE
+
+
+// DELETE
+
+// List all the books 
+// List the data of one book 
+// Create a book (you can send the values using Postman) 
+// Update a book (you can send the values using Postman)
+// Delete a book 
 
 
 const PORT = process.env.PORT || 3000;
